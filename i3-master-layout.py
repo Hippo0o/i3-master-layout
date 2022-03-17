bc9780b7e90f03ac80993e8ad72dc48548f69aa7
Tobias S <seifert@devlabor.com>
2022-03-17 13:36:24 +0100

    trying to fix for sway

--- i3-master-layout.py
+++ i3-master-layout.py
@@ -89,18 +89,56 @@ def move_window(c, subject, target):
     c.command("[con_id=%d] unmark move_target" % target.id)
 
 
+def get_workspace_container(c, focused_window):
+    workspace = focused_window.workspace()
+    sway_workaround(c, focused_window)
+    if (len(workspace.nodes) == 1):
+        return workspace.nodes[0]
+
+    return workspace
+
+
+def sway_workaround(c, focused_window):
+    workspace = focused_window.workspace()
+    if (len(workspace.nodes) <= 1):
+        return
+
+    c.command("[con_id=%d] focus" % workspace.nodes[0].id)
+    c.command("focus parent; move container to workspace current")
+    c.command("[con_id=%d] focus" % focused_window.id)
+
+
+def set_stack_layout(c, workspace_container):
+    if len(workspace_container.nodes) < 2:
+        return
+
+    layout = options.stack_layout or "splitv"
+
+    last = find_last(workspace_container)
+
+    # last window is also 2nd window
+    # splith is not supported yet. idk how to differentiate between splith and nested splith.
+    if last == workspace_container.nodes[1] and (last.layout != layout):
+        c.command("[con_id=%d] split vertical" % last.id)
+        c.command("[con_id=%d] layout %s" % (last.id, layout))
+
+
 def on_window_new(c, e):
     new_window = grab_focused(c)
 
     if new_window is None:
         return
 
-    # only windows created on workspace level get moved if nested option isn't enabled
-    if options.move_nested is not True and new_window.parent != new_window.workspace():
+    workspace_container = get_workspace_container(c, new_window)
+
+    # only windows created on workspace_container level get moved if nested option isn't enabled
+    if options.move_nested is not True and new_window.parent != workspace_container:
         return
 
     # new window gets moved behind last window found
-    move_window(c, new_window, find_last(new_window.workspace()))
+    move_window(c, new_window, find_last(workspace_container))
+
+    set_stack_layout(c, workspace_container)
 
 
 def on_window_focus(c, e):
@@ -109,34 +147,30 @@ def on_window_focus(c, e):
     if focused_window is None:
         return
 
-    workspace = focused_window.workspace()
+    workspace_container = get_workspace_container(c, focused_window)
 
     if options.disable_rearrange is not True:
         # master window disappears and only stack container left
-        if len(workspace.nodes) == 1:
-            # move focused window (usually last focused window of stack) back to workspace level
-            move_window(c, focused_window, workspace)
-            # now the stack if it exists is first node and gets moved to the end of workspace
-            move_window(c, workspace.nodes[0], workspace)
+        if len(workspace_container.nodes) == 1:
+            c.command("[con_id=%d] layout splith" % workspace_container.id)
+            # move focused window (usually last focused window of stack) back to workspace_container level
+            move_window(c, focused_window, workspace_container)
+            # now the stack if it exists is first node and gets moved to the end of workspace_container
+            move_window(c, workspace_container.nodes[0], workspace_container)
 
-    if len(workspace.nodes) < 2:
-        return
+            # set_stack_layout(c, workspace_container)
 
-    layout = options.stack_layout or "splitv"
 
-    last = find_last(workspace)
-
-    # last window is also 2nd window
-    # splith is not supported yet. idk how to differentiate between splith and nested splith.
-    if last == workspace.nodes[1] and (last.layout != layout):
-        c.command("[con_id=%d] split vertical" % last.id)
-        c.command("[con_id=%d] layout %s" % (last.id, layout))
+def on_workspace_focus(c, e):
+    focused_window = grab_focused(c)
+    sway_workaround(c, focused_window)
 
 
 def main():
     c = Connection()
     c.on(Event.WINDOW_FOCUS, on_window_focus)
     c.on(Event.WINDOW_NEW, on_window_new)
+    c.on(Event.WORKSPACE_FOCUS, on_workspace_focus)
 
     try:
         c.main()
