30a053f11f986101e35836ab6c0501a02bff2603
Tobias S <Hippo0o@home>
2024-08-06 23:03:57 +0200

    focus master after swap

--- i3-swap-master.py
+++ i3-swap-master.py
@@ -6,15 +6,21 @@ c = Connection()
 
 tree = c.get_tree()
 focused = tree.find_focused()
-target = focused.workspace().nodes[0]
+workspace = focused.workspace()
+target = workspace.nodes[0]
 
+# if focused is master find target from stack
 def find_last(con):
     if len(con.nodes) > 1:
         return find_last(con.nodes[-1])
 
     return con
-
 if target == focused:
-    target = find_last(focused.workspace());
+    target = find_last(workspace)
 
 c.command("[con_id=%d] swap container with con_id %d" % (focused.id, target.id))
+
+# focus master after swap
+tree = c.get_tree()
+workspace = tree.find_focused().workspace()
+c.command("[con_id=%d] focus" % workspace.nodes[0].id)
